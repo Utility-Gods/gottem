aa100227baf796860061c360f0644aba041b96c2
siddharth <me@siddharthjain.in>
2024-06-27 10:18:18 +0530

    got this

--- release/gottem_v1.2.0/install.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mkdir "%USERPROFILE%\.config\gottem"
-copy gottem_windows_amd64.exe "%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\gottem.exe"
-echo Gottem v1.2.0 installed successfully!
--- release/gottem_v1.2.0/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.config/gottem
-cp gottem_$(uname -s | tr '[:upper:]' '[:lower:]')_amd64 /usr/local/bin/gottem
-chmod +x /usr/local/bin/gottem
-echo "Gottem v1.2.0 installed successfully!"
