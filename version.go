2726a1dcd550a32d79dbe8154a6dc8dd8779b95c
siddharth <me@siddharthjain.in>
2024-06-27 10:18:51 +0530

    Bump version to 1.2.1

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package version
 
 // Version holds the current version of the Gottem application.
 // This is typically updated manually or by a build script before a new release.
-const Version = "1.2.0"
+const Version = "1.2.1"
 
 // Additional version-related information can be added here.
 // For example:
